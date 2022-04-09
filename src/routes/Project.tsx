039985bfb69e96050b80dd7441e0800229aa2dd9
Christopher Robinson <chrispaul_robinson@hotmail.com>
2022-04-09 01:19:13 -0500

    Adjust routes

--- src/routes/Project.tsx
+++ src/routes/Project.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import * as React from 'react';
 
 export interface IAppProps {}
 
 export default function App(props: IAppProps) {
-  return <div></div>;
+  return <div>Project</div>;
 }
