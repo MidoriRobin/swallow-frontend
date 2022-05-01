26bb914fc3eceec9965256f3b2df6f606dfda3e6
Christopher Robinson <chrispaul_robinson@hotmail.com>
2022-05-01 13:27:39 -0500

    Complete base formatting for dashboard page

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -4,8 +4,60 @@ import { useNavigate } from 'react-router-dom';
 
 const DashCont = styled.div`
   /* Layout */
+  width: -webkit-fill-available;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 2rem 0 2rem;
 
   /* Presentation */
+
+  /* sub-classes */
+
+  .other-projects {
+    display: flex;
+    width: -webkit-fill-available;
+    border: solid black 1px;
+    margin: 1rem auto;
+    justify-content: center;
+  }
+`;
+
+const MainProject = styled.div`
+  /* Layout */
+  height: 20rem;
+  display: flex;
+  flex-direction: column;
+  width: -webkit-fill-available;
+  justify-content: flex-start;
+
+  /* Presentation */
+  padding-left: 1rem;
+  border: solid black 1px;
+  margin: 1rem auto;
+`;
+
+const TaskList = styled.div`
+  /* Layout */
+  width: -webkit-fill-available;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  /* Presentation */
+  border: solid black 1px;
+  margin: 1rem auto;
+  padding-left: auto 1rem;
+
+  ul {
+    list-style-type: none;
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    border: solid red 1px;
+  }
 `;
 
 export interface IDashboardProps {}
@@ -74,5 +126,36 @@ export default function Dashboard(props: IDashboardProps) {
    */
   function getBasicUserData() {}
 
-  return <DashCont>Dashboard</DashCont>;
+  return (
+    <DashCont className="dashboard-container">
+      <h4>Dashboard</h4>
+      <MainProject className="main-project">
+        <h4>Name</h4>
+        <p className="desc">Description</p>
+        <p className="date">date created</p>
+        <p className="tasks">Open tasks</p>
+        <p className="requirements">Requirements met</p>
+        <p className="curr-requirement">Current requirement</p>
+      </MainProject>{' '}
+      <TaskList className="task-list">
+        <h4>Tasks</h4>
+        <ul>
+          <li>
+            <h5>Task 1</h5>
+            <p>Description</p> <p>Status</p>
+          </li>
+          <li>
+            <h5>Task 1</h5>
+            <p>Description</p> <p>Status</p>
+          </li>
+          <li>
+            <h5>Task 1</h5>
+            <p>Description</p> <p>Status</p>
+          </li>
+        </ul>
+      </TaskList>{' '}
+      {/* On click should list out a list of other projects, which lead to their project pages  */}
+      <div className="other-projects">Other Projects</div>
+    </DashCont>
+  );
 }
